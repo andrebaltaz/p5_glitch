dd567ca08271a51ceda2eb9b662748df678345ea
andrebaltaz <andrebaltaz@gmail.com>
2025-11-23 15:56:37 +0000

    Update evergreen.js
    Espirógrafo

--- evergreen/sketch.js
+++ evergreen/sketch.js
@@ -1,101 +1,124 @@
-var easycam;
-var cnv;
-let font;
-let textString = 'Feels like Summer.';
+/**
+ * Espirógrafo / Spirograph (Hypotrochoid)
+ * Usa 3 parâmetros clássicos:
+ *  R = raio do círculo fixo
+ *  r = raio do círculo rolante
+ *  d = distância do ponto ao centro do círculo rolante
+ */
 
-var size;
+let RSlider, rSlider, dSlider, speedSlider;
+let t = 0;
 
-function preload(){
-  
-  img = loadImage('https://cdn.glitch.global/a9d45bb7-0399-47e1-96d0-2814590dd3e9/2k_earth_daymap.jpeg?v=1696338828338');
-  font = loadFont('https://cdn.glitch.global/0416fcb2-169f-4a66-963d-92152d1cb0ec/textlet.otf?v=1666262742834');
-  
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  pixelDensity(2);
+  background(0);
+
+  // UI
+  RSlider = makeSlider("R (fixo)", 20, 400, 220, 1, 20);
+  rSlider = makeSlider("r (rolante)", 5, 200, 80, 1, 60);
+  dSlider = makeSlider("d (offset)", 0, 300, 120, 1, 100);
+  speedSlider = makeSlider("velocidade", 0.1, 5, 1.0, 0.1, 140);
+
+  stroke(255);
+  noFill();
 }
 
+function draw() {
+  // fade suave por frame (para ver o desenho a crescer)
+  background(0, 10);
 
-function setup() { 
-  createCanvas(windowWidth, windowHeight, WEBGL);
-  setAttributes('antialias', true);
-  
-  easycam = createEasyCam({distance:300});
-    easycam.setRotation([-0.06558126008011236, -0.0055834802578086954, -0.9818612443429126, 0.1778100670150725]);
+  translate(width / 2, height / 2);
 
-   cnv = createGraphics(400, 400);
-  cnv.background(img);
+  let R = RSlider.slider.value();
+  let r = rSlider.slider.value();
+  let d = dSlider.slider.value();
+  let speed = speedSlider.slider.value();
+
+  // Desenhar curva acumulada
+  strokeWeight(1.2);
+  beginShape();
+  for (let a = 0; a < t; a += 0.01) {
+    let p = spiroPoint(R, r, d, a);
+    vertex(p.x, p.y);
+  }
+  endShape();
+
+  // Ponto atual (cursor)
+  let pNow = spiroPoint(R, r, d, t);
   noStroke();
-  
-  size=windowWidth/16;
-  
-} 
+  fill(255);
+  circle(pNow.x, pNow.y, 5);
 
+  // Avança o tempo
+  t += 0.01 * speed;
+
+  // Reset quando fecha o ciclo (aproximação)
+  if (t > TWO_PI * lcm(R, r) / r) {
+    t = 0;
+    background(0);
+  }
+
+  drawLabels();
+}
+
+function spiroPoint(R, r, d, a) {
+  // hypotrochoid:
+  // x = (R - r) cos a + d cos((R - r)/r * a)
+  // y = (R - r) sin a - d sin((R - r)/r * a)
+  let k = (R - r) / r;
+
+  let x = (R - r) * cos(a) + d * cos(k * a);
+  let y = (R - r) * sin(a) - d * sin(k * a);
+
+  return createVector(x, y);
+}
+
+/* ---------- UI helpers ---------- */
+
+function makeSlider(label, min, max, val, step, y) {
+  let container = createDiv("");
+  container.position(20, y);
+  container.style("color", "#ddd");
+  container.style("font-family", "monospace");
+  container.style("font-size", "12px");
+
+  let lab = createSpan(label + " ");
+  lab.parent(container);
+
+  let s = createSlider(min, max, val, step);
+  s.parent(container);
+  s.style("width", "220px");
+
+  let readout = createSpan(" " + val);
+  readout.parent(container);
+
+  return { container, label: lab, slider: s, readout };
+}
+
+function drawLabels() {
+  let sliders = [RSlider, rSlider, dSlider, speedSlider];
+  for (let obj of sliders) {
+    obj.readout.html(" " + nf(obj.slider.value(), 1, 1));
+  }
+}
+
+/* --- LCM approx for cycle reset --- */
+function gcd(a, b) {
+  a = floor(a); b = floor(b);
+  while (b !== 0) {
+    let tmp = b;
+    b = a % b;
+    a = tmp;
+  }
+  return a;
+}
+function lcm(a, b) {
+  a = floor(a); b = floor(b);
+  return abs(a * b) / gcd(a, b);
+}
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  easycam.setViewport([0,0,windowWidth, windowHeight]);
-  easycam.rotation(100,0,0,0);
-}
-
-
-function draw(){
- 
-  // projection
-  perspective(60 * PI/180, width/height, 1, 5000);
-  
-  // BG
-  background(32);
-  
-  // objects
-  strokeWeight(0.5);
-  stroke(0);
-  
-  push();
-  //translate(50, 50, 0);
-  
-  cnv.fill(255,random(5,210),random(5,20), random(50, 200));
-  cnv.noStroke()
-  cnv.circle(random(width), random(height), random(5, 20))
-  
-  fill(255);
-  texture(cnv);
-  // texture(img);
-  
-  rotateY(frameCount * 0.003);
-  sphere(size,50);
- //easycam.setRotation(frameCount * 0.003,0,0,0);
-  //box(150, 150, 150);
-textFont(font);
-  textSize(size/4);
-  textAlign(CENTER);
-  text(textString, 0, -size-10);
-  pop();
-  /*
-  push();
-  translate(-50, -50, 0);
-  fill(255,0,128);
-  box(50, 50, 25);
-  pop();
-  
-  push();
-  translate(+50, -50, 0);
-  fill(0,128,255);
-  box(50, 50, 25);
-  pop();
-  
-  push();
-  translate(-50, +50, 0);
-  fill(255,255,0);
-  box(50, 50, 25);
-  pop();*/
-
-}
-
-function mousePressed(){
-   cnv.background(img); 
-   
-}
-
-function mouseReleased(){
-   
-    easycam.setRotation([-0.06558126008011236, -0.0055834802578086954, -0.9818612443429126, 0.1778100670150725]);
-
+  background(0);
 }
